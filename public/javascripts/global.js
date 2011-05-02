76b994cfb1f2df47ac8f13d092fddba22f475623
RamonPage <ramonpage@gmail.com>
2011-05-02 01:52:38 -0300

    Removing 2010 files.

--- public/javascripts/global.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  $("ul#tabs li a").click(function(e) {
-    e.preventDefault();
-    $(".tab-content").fadeOut("fast");
-    $("#"+$(this).attr("id").replace("nav_","")).fadeIn("fast");
-    $("ul#tabs li a").removeClass("active");
-    $(this).addClass("active");
-  });
-});
